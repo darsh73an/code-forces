a8732ca1df2b5a9b9239beb8c5c501d0d5091116
darsh73an <darshankok7@gmail.com>
2026-09-15 09:32:49 +0530

    continue fo implmentation in codeforces

---
+++ 4A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int w;
+    cout <<" Enter weight of watermelon :";
+    cin >> w;
+    
+    if(w > 2 && w % 2 == 0){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
