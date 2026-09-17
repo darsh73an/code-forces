215baead949cd5ccb289ad6dc892178b4d7e8cf1
darsh73an <darshankok7@gmail.com>
2026-09-17 15:11:08 +0530

    solved some unique problems on codeforces

---
+++ 236A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+int main (){
+    string s;
+    cin >> s;
+    
+    unordered_set<char> st(s.begin(),s.end());
+    
+    if(st.size() % 2 == 0){
+        cout << "CHAT WITH HER!";
+    }else{
+        cout << "IGNORE HIM!";
+    }
+}
+
+// st.size()
+
+// gives the number of distinct characters.
